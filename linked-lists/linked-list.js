f0266ebae464259c1aa7416ca6cffa83b172b896
Lee Broxson <broxsonl@gmail.com>
2016-12-09 15:42:16 -0800

    Created linkedlist and node objects files.

---
+++ linked-lists/linked-list.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = function() {
+  this.head = null;
+  this.length = 0;
+};
---
+++ linked-lists/node.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = function() {
+  this.value = null;
+  this.next = null;
+};
