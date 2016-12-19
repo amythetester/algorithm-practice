ee8e0a379ac17d4877283030b6be9ff1cfacb31a
Lee Broxson <broxsonl@gmail.com>
2016-12-18 19:34:13 -0800

    Re-wrote stacks as queues for practice.

--- stack-practice/queue-with-stacks2.js
+++ stack-practice/queue-with-stacks2.js
@@ -14,9 +14,20 @@ Array.prototype.peek = function() {
   return this[0];
 };
 
-function enqueue(input) {
-  stack1.push(input);
+function enqueue(num) {
+  stack1.push(num);
 }
+
+function dequeue() {
+
+  if (stack2.isEmpty()) {
+    while (!stack1.isEmpty()) {
+      stack2.push(stack1.pop());
+    }
+  }
+  return stack2.shift();
+}
+
 
 enqueue(1);
 enqueue(2);
@@ -24,25 +35,6 @@ enqueue(3);
 enqueue(4);
 enqueue(5);
 
-
-function dequeue() {
-
-  if (stack2.isEmpty() && stack1.isEmpty()) throw new Error('Nothing to dequeue');
-
-  if (!stack2.isEmpty()) {
-    return stack2.shift();
-  }
-  else {
-    while (!stack1.isEmpty()) {
-      stack2.push(stack1.pop());
-    }
-
-    return stack2.shift();
-  }
-}
-
-console.log(stack1);
-console.log(dequeue());
 console.log(dequeue());
 console.log(dequeue());
 console.log(dequeue());
