bd188812606028659d61cad8d42d6e4434569bee
Lee Broxson <broxsonl@gmail.com>
2016-12-26 18:53:24 -0800

    Implemented own binary search.

--- binary-search-trees/add-new-node.js
+++ binary-search-trees/add-new-node.js
@@ -3,31 +3,46 @@
 const BST = require('./tree.js');
 const Node = require('./node.js');
 
-module.exports = BST.prototype.addNewNode = function(value) {
+let testTree = new BST();
+testTree.addNewNode(10);
+testTree.addNewNode(8);
+testTree.addNewNode(11);
+testTree.addNewNode(7);
+testTree.addNewNode(9);
+testTree.addNewNode(4);
+testTree.addNewNode(10);
+testTree.addNewNode(19);
 
-  let tempNode = new Node(value);
+function findSum(tree, target) {
 
-  if (!this.head) {
-    this.head = tempNode;
-    return;
+  let path = [];
+
+  function getSum(node, currentSum) {
+    node.aggregatedSum = currentSum + node.value;
+    if (node.aggregatedSum === target) {
+      node.validPath = true;
+      return true;
+    }
+    if (!node.validPath && node.left) node.validPath = getSum(node.left, node.aggregatedSum);
+    if (!node.validPath && node.right) node.validPath = getSum(node.right, node.aggregatedSum);
+    return node.validPath;
   }
 
-  let current = this.head;
-
-  while(current) {
-    if (tempNode.value < current.value) {
-      if (!current.left) {
-        current.left = tempNode;
-        return;
-      }
-      current = current.left;
+  function findPath(node) {
+    while (node && node.validPath) {
+      path.push(node.value);
+      if (node.left && node.left.validPath) node = node.left;
+      else if (node.right && node.right.validPath) node = node.right;
+      else node = null;
     }
-    if (tempNode.value >= current.value) {
-      if (!current.right) {
-        current.right = tempNode;
-        return;
-      }
-      current = current.right;
-    }
+    return path;
   }
-};
+
+  if (getSum(tree.root, 0)) {
+    console.log('hit');
+    return findPath(tree.root);
+  }
+  return false;
+}
+
+console.log(findSum(testTree, 18));
--- binary-search-trees/binary-search.js
+++ binary-search-trees/binary-search.js
@@ -16,15 +16,29 @@
 // Need to perform a search first that looks to the left, right and on the index to determine if the number is smaller or larger to the left, etc to figure out if it is incorrectly smaller/larger or whatever and
 // then run another search within the bounds of that chunk
 
-let testArray = [6, 7, 8, 9, 1, 2, 3, 4, 5];
+let testArray = [-5, -1, 0, 3, 5, 8, 12, 15];
 
-function binarySearch(n, array) {
+function binarySearch(arr, n) {
+  let mid;
 
-  let chunkStart;
+  return _binarySearch(0, arr.length - 1);
 
-  for (var i = 0; i < array.length; i++) {
-    if (array[i] < array[i - 1]) chunkStart = array[i - 1];
+  function _binarySearch(arrStart, arrEnd) {
+    mid = Math.floor((arrStart + arrEnd) / 2);
+
+    console.log(arr[mid]);
+    console.log(n);
+
+    if (arr[mid] === n) {
+      return mid;
+    }
+
+    if (arrStart === arrEnd && arr[mid] !== n) return false;
+
+    if (n > arr[mid]) return _binarySearch(mid + 1, arrEnd);
+    if (n < arr[mid]) return _binarySearch(arrStart, mid - 1);
+    return;
   }
-
-
 }
+
+console.log(binarySearch(testArray, 0));
