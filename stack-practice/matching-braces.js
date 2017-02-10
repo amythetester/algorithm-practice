bd2a10190b61253754b04bcf9bf275128d01539a
Lee Broxson <broxsonl@gmail.com>
2017-02-09 19:04:48 -0800

    Refactored brace matching.

--- stack-practice/matching-braces.js
+++ stack-practice/matching-braces.js
@@ -17,8 +17,7 @@ function validBraces(braceStr) {
     stack.push(braceStr[i]);
   }
 
-  if (stack.length > 0) return false;
-  return true;
+  return (stack.length > 0) ? false : true;
   // Every time we come across an open brace, we'll want to push it into stack1.
   // If it's a closing brace, we compare it to the top of the stack and pop it off if it is the same as the prior item in the stack.
 }
