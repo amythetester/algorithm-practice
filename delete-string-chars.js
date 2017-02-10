3afcf385fd54a5dc80996897ddd1e0f6d8d72131
Lee Broxson <broxsonl@gmail.com>
2017-02-09 20:40:58 -0800

    Wrote method to delete characters in a string given an array of chars to delete.

---
+++ delete-string-chars.js
@@ -0,0 +1,23 @@
+'use strict';
+
+function deleteStrChars(charArr, str) {
+
+  let charObj = {};
+
+  for (let i = 0; i < charArr.length; i++) charObj[charArr[i]] = true;
+
+  let newStr = '';
+
+  for (let i = 0; i < str.length; i++) {
+    if (charObj.hasOwnProperty(str[i])) continue;
+
+    newStr = newStr + str[i];
+  }
+  return newStr;
+}
+
+let testDeletes = ['a', 'b', 'c'];
+
+let testStr = 'abbreviate the cat';
+
+console.log(deleteStrChars(testDeletes, testStr));
